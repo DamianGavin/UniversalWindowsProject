a723adf99a9e72265e8f4bddd18c208b519a8f0f
DamianGavin <10007969@gmit.ie>
2018-02-28 14:49:37 +0000

    Elipses drawn

---
+++ UniversalWindowsProject/Model-simon/Quadrant.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Shapes;
+
+namespace Simon.Model
+{
+    class Quadrant
+    {
+        private Ellipse shape;
+        // Sound
+
+        public void MakeNoise()
+        {
+
+        }
+
+        public void Brighten()
+        {
+
+        }
+
+    }
+}
---
+++ UniversalWindowsProject/Model-simon/Simon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simon.Model
+{
+    class Simon
+    {
+        private List<Quadrant> history;
+        private List<Quadrant> quadrants;
+
+
+        public int TurnNo => history.Count;
+
+
+        public Quadrant GetAt(int i)
+        {
+            return quadrants[i];
+        }
+
+        public Simon(List<Quadrant> quadrants)
+        {
+            this.history = new List<Quadrant>();
+            this.quadrants = quadrants;
+        }
+
+
+        public Boolean DoesMatch(List<int> choices)
+        {
+            return false;
+        }
+
+
+        public void Play()
+        {
+            history.Add(NextQuadrant());
+
+            // do stuff wtith history
+            foreach (var q in history)
+            {
+                q.MakeNoise();
+                q.Brighten();
+            }
+        }
+
+        public void Reset()
+        {
+            history.Clear();
+        }
+
+        private Quadrant NextQuadrant()
+        {
+            // choose random element from quadrants
+            throw new System.NotImplementedException();
+        }
+    }
+}
