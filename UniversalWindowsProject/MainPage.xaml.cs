ef6b8ce12ec8bfb8a37b4e666371ec00f9e193b3
DamianGavin <10007969@gmit.ie>
2018-03-01 00:23:15 +0000

    Music implemented

--- UniversalWindowsProject/MainPage.xaml.cs
+++ UniversalWindowsProject/MainPage.xaml.cs
@@ -12,19 +12,43 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Media.Playback;
+using Windows.Media.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace UniversalWindowsProject
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+	    MediaPlayer player;
+	    bool playing;
         public MainPage()
         {
             this.InitializeComponent();
+			player = new MediaPlayer();
+	        playing = false;
         }
+
+	    private async void Button_click(object sender, RoutedEventArgs e)
+	    {
+		    Windows.Storage.StorageFolder folder =
+			    await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
+		    var file = await folder.GetFileAsync("3.mp3");
+
+		    player.AutoPlay = false;
+		    player.Source = MediaSource.CreateFromStorageFile(file);
+
+		    if (playing)
+		    {
+			    player.Source = null;
+			    playing = false;
+		    }
+		    else
+		    {
+			    player.Play();
+			    playing = true;
+		    }
+	    }
     }
 }
